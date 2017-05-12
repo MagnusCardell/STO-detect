6fca8a76bce8c72dbec5e1a29f4de0cd37ea28b3
Magnus Cardell <cardell.magnus@gmail.com>
2017-05-11 22:35:36 -0500

    fixing pca

--- STO-detect/Source.cpp
+++ STO-detect/Source.cpp
@@ -9,11 +9,199 @@ using namespace std;
 using namespace cv;
 
 int threshold_val = 127;
+/**Function readimage
+1 argument - string: file
+-Read .jpg file associated with that filename and store in a matrix
+Return matrix - Mat: image
+*/
+Mat readimage(string file) {
+	Mat image = imread(file, CV_LOAD_IMAGE_GRAYSCALE);
+	if (image.empty()) { //Checkpoint
+		cout << "Error [image read in] : Image cannot be loaded" << endl;
+	}
+	return image;
+}
 
 struct Hand_coordinates {
 	int x_co;
 	int y_co;
 };
+static  Mat formatImagesForPCA(const vector<Mat> &data) {
+	Mat dst(static_cast<int>(data.size()), data[0].rows*data[0].cols, CV_32F);
+	for (unsigned int i = 0; i < data.size(); i++) {
+		Mat image_row = data[i].clone().reshape(1, 1);
+		Mat row_i = dst.row(i);
+		image_row.convertTo(row_i, CV_32F);
+	}
+	return dst;
+}
+
+
+// Normalizes a given image into a value range between 0 and 255.
+//	Given the address to the matrix.
+Mat norm_0_255(const Mat& src) {
+	// Create and return normalized image:
+	Mat dst;
+	switch (src.channels()) {
+	case 1:
+		cv::normalize(src, dst, 0, 255, NORM_MINMAX, CV_8UC1);
+		break;
+	case 3:
+		cv::normalize(src, dst, 0, 255, NORM_MINMAX, CV_8UC3);
+		break;
+	default:
+		src.copyTo(dst);
+		break;
+	}
+	return dst;
+}
+
+vector<Mat> face_capture(Mat &frame, CascadeClassifier &face_cascade) {
+	vector<Rect> faces; //collection of rectangle sizes
+	vector<Mat> faces_comp; //collection of faces
+	face_cascade.detectMultiScale(frame, faces, 1.1, 3, CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_SCALE_IMAGE, Size(100, 100)); //For better performance set min face size to 100x100
+	while (faces_comp.size() < 10) {
+		for (int i = 0; i < faces.size(); i++) {
+			Point pt1(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
+			Point pt2(faces[i].x, faces[i].y);
+			//rectangle(frame, pt1, pt2, cvScalar(0, 255, 0, 0), 1, 0, 0); //Rectangle around the face
+
+
+			Rect face = Rect(pt1.x - faces[i].width, pt1.y - faces[i].height, faces[i].width, faces[i].height);
+			Mat roi = frame(face);
+			resize(roi, roi, Size(50, 50));
+			imshow("riu1," + i, roi);
+			faces_comp.push_back(roi);
+		}
+	}
+	cout << faces_comp.size() << endl;
+	
+	//cvWaitKey(0);
+	return faces_comp;
+}
+
+Mat *face_processing(vector<Mat> train) {
+
+	const int numberofsets = 1;
+
+	Mat image = train[0];
+	Mat average[numberofsets];
+	//Vector of Matrices that will collect all images
+	vector<Mat> faces[numberofsets];
+	//Get the covariance matrix
+	Mat combine[numberofsets];
+	Mat eigenval[numberofsets], eigenvect[numberofsets];
+	Mat top4vectors[numberofsets];
+	vector<Mat> eigenfacesimage[numberofsets];
+	vector<Mat> eigenfacesvector[numberofsets];
+	vector<Mat> imageprojection[numberofsets];
+	vector<Mat> vectprojection[numberofsets];
+	vector<float>euclidiandist[numberofsets];
+	float lowest[numberofsets];
+	
+	for (int seton = 0; seton < numberofsets; seton++) {
+		average[seton] = Mat::zeros(image.size(), image.type());
+		//empty 2D array for accumulating pixel values over 255 and computing average
+		int **collect;
+		collect = new int*[50];
+		for (int x = 0; x < 50; ++x) {
+			collect[x] = new int[50];
+			for (int y = 0; y < 50; ++y) {
+				collect[x][y] = 0;
+			}
+		}
+		//read the rest of the images to add them up pixel by pixel to the collect array
+		int s = 0;
+		for (int i = (1 + (10 * seton)); i <= (10 + (10 * seton)); ++i) {
+			//1. read in the file
+			
+			image = train[i];
+			//2. collect total values
+			for (int n = 0; n < 50; n++) {
+				for (int k = 0; k < 50; k++) {
+					//Target pixel value and store it in collect array to perform pixel operations above 255
+					// Scalar type is a 3-channel data type. We just want the first channel
+					Scalar intensity = image.at<uchar>(n, k);
+					collect[n][k] += intensity.val[0];
+				}
+			}
+			//store image in vector
+			faces[seton].push_back(image);
+			//3. input average back into one image to get the AVERAGE face
+		}
+
+		for (int n = 0; n < 50; n++) {
+			for (int k = 0; k < 50; k++) {
+				//Scalar type is necessary to keep the 3-channel data type of a Matrix
+				Scalar intensity2 = average[seton].at<uchar>(n, k);
+				intensity2.val[0] = (collect[n][k]) / 10;
+				average[seton].at<uchar>(n, k) = intensity2.val[0];
+			}
+		}
+		//subtract average faces from every face:
+		for (int i = 0; i < 10; i++) {
+			faces[seton][i] -= average[seton];
+			//imshow("window" + i, faces[i]);
+			//imwrite("featureface" + to_string(i) + ".jpg", faces[0][i]);
+		}
+
+
+		combine[seton] = formatImagesForPCA(faces[seton]); //function for pushing all images into one matrix
+
+
+		  //not that useful
+		 /*
+	   Mat covariance, mean2;
+	   //OpenCV function for getting the covariance matrix. Flag specify rows or columns
+	   calcCovarMatrix(combine, covariance, mean2, COVAR_ROWS | cv::COVAR_NORMAL);
+		//cout << "covriance = " << endl << " " << covariance << endl << endl;
+		cout << covariance.size() << endl;
+		imwrite("Covariance.jpg", covariance);
+		 */
+
+		 //Get Egienvalues and eigenvectors
+		PCA pca(combine[seton], Mat(), PCA::DATA_AS_ROW, 10);
+		eigenval[seton] = pca.eigenvalues;
+		eigenvect[seton] = pca.eigenvectors;
+
+
+
+
+		//cout << "eigenval = " << endl << " " << eigenval << endl << endl;
+		//cout << "eigenvect = " << endl << " " << eigenvect << endl << endl;
+		//cout << "Eigenval size: " << eigenval.size() << " Eigenvect size: " << eigenvect.size() << endl;
+
+
+		//Get the top 4 vectors from top 4 eigenvalues
+
+		top4vectors[seton].push_back(eigenvect[seton].row(0));
+		top4vectors[seton].push_back(eigenvect[seton].row(1));
+		top4vectors[seton].push_back(eigenvect[seton].row(2));
+		top4vectors[seton].push_back(eigenvect[seton].row(3));
+		cout << "AEKBAFE" << endl;
+
+
+		for (int n = 0; n < 10; n++) {
+			for (int i = 0; i < 4; i++) {
+				//Nth row of (face-average) x ith row of eigenvector by component multiplcication .mul()
+				Mat eigenfacevector[numberofsets];
+				eigenfacevector[seton] = combine[seton].row(n).mul(top4vectors[seton].row(i));
+				eigenfacesimage[seton].push_back(norm_0_255(eigenfacevector[seton]).reshape(1, faces[seton][0].rows));
+				eigenfacesvector[seton].push_back(eigenfacevector[seton]);
+			}
+		}
+
+		//cout << "The amount of Eigenfaces are: " << eigenfacesimage[seton].size() << endl;
+		/*
+		for (int i = 0; i < eigenfacesimage[seton].size(); i++) {
+		imwrite("eigenface" + to_string(i) + ".jpg", eigenfacesimage[seton][i]);
+		}
+		*/
+	}
+	return top4vectors;
+}
+
+
 /*	Setup function.
 	- creates window and trackbar to adjust threshold
 */
@@ -63,7 +251,6 @@ Hand_coordinates draw_palm_roi(Mat &frame) {
 	- collect faces with Haar Cascade analysis.
 */
 vector<Mat> facedetection(Mat &frame, CascadeClassifier &face_cascade) {
-	Mat gray; //Frame where the faces will reside
 	vector<Rect> faces; //collection of rectangle sizes
 	vector<Mat> faces_comp; //collection of faces
 	face_cascade.detectMultiScale(frame, faces, 1.1, 3, CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_SCALE_IMAGE, Size(100, 100)); //For better performance set min face size to 100x100
@@ -86,7 +273,6 @@ vector<Mat> facedetection(Mat &frame, CascadeClassifier &face_cascade) {
 		//findContours(skin, skin, 1, 1 );
 		imshow("WOw", skin);
 		*/
-		Rect roi_ = Rect(50, 50, 250, 250);
 		Rect face = Rect(pt1.x-faces[i].width, pt1.y-faces[i].height, faces[i].width, faces[i].height);
 		Mat roi = frame(face);
 		resize(roi, roi, Size(50, 50));
@@ -123,10 +309,10 @@ int main() {
 	CascadeClassifier face_cascade;	//create cascade classifier used for the face detection
 	setup_window(face_cascade);
 
-
-
+	vector<Mat> train_faces = face_capture(frame, face_cascade);
+	Mat *eigenfaces = face_processing(train_faces);
 	// start capture
-	start_capture(cap, frame, face_cascade);
+	//start_capture(cap, frame, face_cascade);
 
 	return 0;
 }
